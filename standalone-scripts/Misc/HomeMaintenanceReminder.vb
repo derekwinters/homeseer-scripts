ec4950fe65b88ff9d5cb04cea84aa99c8abd8cf5
Derek Winters <contact@derekwinters.com>
2017-05-08 22:25:25 -0500

    Made script re-usable

--- standalone-scripts/Misc/HomeMaintenanceReminder.vb
+++ standalone-scripts/Misc/HomeMaintenanceReminder.vb
@@ -4,20 +4,23 @@
 ' Send a variety of pre-configured home maintenance reminders.
 ' ==============================================================================
 Sub Main(Param as Object)
-	' ==========================================================================
-	' Create Variables
-	' ==========================================================================
-	Dim AmyCell   As String = "7637426359@tmomail.net"
-	Dim DerekCell As String = "7632184538@mms.att.net"
-	Dim SendFrom  As String = "home@derekwinters.com"
-	Dim Message   As String = ""
-	
-	' ==========================================================================
-	' Build Message
-	' ==========================================================================
-	If ( Param = "furnace filter" ) Then
-		Message = "<br />Change the furnace filter today."
-	End If
+    ' ==========================================================================
+    ' Create Variables
+    ' ==========================================================================
+    Dim Message As String = ""
+
+    ' ==========================================================================
+    ' Build Message
+    ' ==========================================================================
+    ' A simple string can be passed via the parameter, or a trigger word can be
+    ' passed that has a pre-configured rich-message prepared.
+    If (Param = "furnace filter") Then
+        Message = "<br />Change the furnace filter today."
+    ElseIf (Param = "garbage") Then
+        Message = "<br />Garbage comes tomorrow."
+    ElseIf (Param <> "") Then
+        Message = Param
+    End If
 	
 	' ==========================================================================
 	' Send message
@@ -25,8 +28,8 @@ Sub Main(Param as Object)
 	If ( Message = "" ) Then
 		hs.WriteLog("Maintenance Reminder","Maintenance reminder failed, invalid parameter passed to function.")
 	Else
-		hs.SendEmail(DerekCell,SendFrom ,"", "", "Maintenance Reminder", Message, "")
-		hs.SendEmail(AmyCell,  SendFrom ,"", "", "Maintenance Reminder", Message, "")
-	End If
+        hs.SendEmail(hs.DeviceString(168), hs.DeviceString(174), "", "", "Maintenance Reminder", Message, "")
+        hs.SendEmail(hs.DeviceString(167), hs.DeviceString(174), "", "", "Maintenance Reminder", Message, "")
+    End If
 	
 end Sub
