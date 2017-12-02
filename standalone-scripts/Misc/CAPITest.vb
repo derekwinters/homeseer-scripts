977f73b4e2e9499fe2e13efae4e35a3ee29ede03
Derek Winters <github@derekwinters.com>
2017-12-02 01:11:00 -0600

    added CAPITest

---
+++ standalone-scripts/Misc/CAPITest.vb
@@ -0,0 +1,25 @@
+﻿Sub Main(ByVal strDevRef As String)
+	Dim objDev As Object
+	Dim intDevRef as Integer
+	Dim intCount As Integer
+
+	intDevRef = CInt(strDevRef)	
+	objDev = hs.GetDevicebyRef(intDevRef)
+	
+	If objDev Is Nothing Then
+		hs.WriteLog("DevInfo","Invalid device ref: " & strDevRef )
+	Else
+		hs.WriteLog("DevInfo", "Ref=" & strDevRef & "; Name=" & objDev.Name(hs) & "; Location=" & objDev.Location(hs) & "; Location2=" & objDev.Location2(hs) & "; Address=" & objDev.Address(hs))
+	
+		intCount = 0
+		
+		For Each objCAPIControl As CAPIControl In hs.CAPIGetControl(intDevRef)
+			hs.WriteLog("DevInfo", ".CCIndex=" & CStr(objCAPIControl.CCIndex) & "; Label=" & CStr(objCAPIControl.Label) & "; ControlType=" & CStr(objCAPIControl.ControlType) & "; ControlValue=" & CStr(objCAPIControl.ControlValue) & "; ControlString=" & objCAPIControl.ControlString)
+			intCount = intCount + 1
+		Next
+		
+		If intCount = 0 Then
+			hs.WriteLog("DevInfo", "No CAPIControl objects defined for this device")
+		End If
+	End If
+End Sub
