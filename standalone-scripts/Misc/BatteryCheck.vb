5f4e6765649ba75ab38dfc0fe9b143a97d7b8e0a
Derek Winters <github@derekwinters.com>
2017-12-02 20:05:37 -0600

    New function

--- standalone-scripts/Misc/BatteryCheck.vb
+++ standalone-scripts/Misc/BatteryCheck.vb
@@ -22,20 +22,60 @@ Sub Main(Parms As Object)
             Continue Do
         End If
 
-        If (Device.Device_Type_String(hs) = "Z-Wave Battery") Then
-            hs.WriteLog("HVAC Automation", "Found a battery device (ReferenceID: " & Device.ref(hs) & ", Value: " & hs.DeviceValue(Device.ref(hs)) & ")")
-            If (hs.DeviceValue(Device.ref(hs)) < 15) Then
-                Body = Body & Device.Location(hs) & " " & Device.Name(hs) '& ": " & Device.Value(hs)
-                Total = Total + 1
-            End If
-
+        If (Device.Device_Type_String(hs) = "Z-Wave Battery") Then			
+			hs.WriteLog("HVAC Automation", "Found a battery device (ReferenceID: " & Device.ref(hs) & ", Value: " & hs.DeviceValue(Device.ref(hs)) & ")")
+			If (hs.DeviceValue(Device.ref(hs)) < 15 And Device.ref(hs) <> 87 ) Then
+				Body = Body & Device.Location(hs) & " " & Device.Name(hs) & ": " & hs.DeviceValue(Device.ref(hs))
+				Total = Total + 1
+			End If
+			
         End If
 
     Loop
 
     If (Total > 0) Then
-        hs.SendEmail(hs.DeviceString(168), hs.DeviceString(174), "", "", "Battery Alert", Body, "")
-        'hs.SendEmail(hs.DeviceString(167), hs.DeviceString(174), "", "", "Battery Alert", Body, "")
+        SendMessage("Battery Alert",Body)
     End If
 
+End Sub
+
+' ==============================================================================
+' Send Message
+' ==============================================================================
+Sub SendMessage(SubjectString As String, MessageString As String)
+
+    ' Set up enumerator
+	Dim Device As Scheduler.Classes.DeviceClass
+	Dim Enumerator As Scheduler.Classes.clsDeviceEnumeration
+
+	' Start
+	Enumerator = hs.GetDeviceEnumerator
+
+	' Check
+	If (Enumerator Is Nothing) Then
+		hs.WriteLog("MMS Messaging", "Error getting enumerator")
+		Exit Sub
+	End If
+
+	' Loop and send messages
+	Do While Not Enumerator.Finished
+		Device = Enumerator.GetNext
+
+		If (Device Is Nothing) Then
+			Continue Do
+		End If
+
+		' If the device type string is MMSPhoneNumber and the device is on,
+		' send the message. Checking if the device is off allows for easily
+		' disabling sending to a specific number without modifying scripts.
+		If (Device.Device_Type_String(hs) = "MMSPhoneNumber") Then
+			If (hs.DeviceValue(Device.ref(hs)) = 100) Then
+				hs.SendEmail(hs.DeviceString(Device.ref(hs)), hs.DeviceString(174), "", "", SubjectString, MessageString, "")
+			Else
+				hs.WriteLog("MMS Messaging", "Device is disabled for messaging (ReferenceID: " & Device.ref(hs) & ")")
+			End If
+		End If
+
+	Loop
+	
 End Sub
