63f4ffc9af7e97ea054c524855547bdea6964c7b
Derek Winters <github@derekwinters.com>
2017-12-07 19:51:21 -0600

    standalone SendMessage script

---
+++ standalone-scripts/Misc/SendMessage.vb
@@ -0,0 +1,44 @@
+﻿' ==============================================================================
+' Send Message
+' ==============================================================================
+Sub Main(Parm As Object)
+
+	Dim Parms() As String  = Split(Parm.ToString,",")
+	Dim SubjectString As String  = Parms(0)
+	Dim MessageString As String  = Parms(1)
+
+    ' Set up enumerator
+	Dim Device As Scheduler.Classes.DeviceClass
+	Dim Enumerator As Scheduler.Classes.clsDeviceEnumeration
+
+	' Start
+	Enumerator = hs.GetDeviceEnumerator
+
+	' Check
+	If (Enumerator Is Nothing) Then
+		hs.WriteLog("MMS Messaging", "Error getting enumerator")
+		Exit Sub
+	End If
+
+	' Loop and send messages
+	Do While Not Enumerator.Finished
+		Device = Enumerator.GetNext
+
+		If (Device Is Nothing) Then
+			Continue Do
+		End If
+
+		' If the device type string is MMSPhoneNumber and the device is on,
+		' send the message. Checking if the device is off allows for easily
+		' disabling sending to a specific number without modifying scripts.
+		If (Device.Device_Type_String(hs) = "MMSPhoneNumber") Then
+			If (hs.DeviceValue(Device.ref(hs)) = 100) Then
+				hs.SendEmail(hs.DeviceString(Device.ref(hs)), hs.DeviceString(174), "", "", SubjectString, MessageString, "")
+			Else
+				hs.WriteLog("MMS Messaging", "Device is disabled for messaging (ReferenceID: " & Device.ref(hs) & ")")
+			End If
+		End If
+
+	Loop
+	
+End Sub
