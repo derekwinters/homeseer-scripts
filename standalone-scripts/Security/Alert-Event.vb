eb2fde91a71d462968a9c130c66aac30f6c4fe69
Derek Winters <git@derekwinters.com>
2019-07-28 00:07:55 -0500

    fix indentation

--- standalone-scripts/Security/Alert-Event.vb
+++ standalone-scripts/Security/Alert-Event.vb
@@ -5,96 +5,96 @@
 ' 
 ' ==============================================================================
 Sub Main(parms As Object)
-	
-	' Wait to allow time if someone just got home
-	hs.WaitSecs(10)
 
-	' ======================================================================
-	' Variables
-	' ======================================================================
-	Dim Body  As String = ""
-	Dim Alarm as Boolean = False
-	
-	' ==========================================================================
-	' Initial Checks
-	' ==========================================================================
-	' If Security Current Mode <> Disarmed
-	If ( hs.DeviceValue("28") <> 0 ) Then
-		
-		' ======================================================================
-		' Armed and Perimeter Devices
-		' ======================================================================
-		' These devices will always trigger an alarm if they are changed when
-		' the system is not disarmed.
-		' ======================================================================
-		' Check the garage door
-		' ======================================================================
-		If ( hs.DeviceValue("72") <> 0 ) Then
-			Body = Body & " - Garage door is open - <br />"
-			Alarm = True
-		Else
-			Body = Body & "Garage door is closed<br />"
-		End If
+  ' Wait to allow time if someone just got home
+  hs.WaitSecs(10)
 
-        ' ======================================================================
-        ' Check the front door
-        ' ======================================================================
-        If (hs.DeviceValue("201") = 255) Then
-            Body = Body & "Front door is locked<br />"
-        Else
-            Body = Body & " - Front door is unlocked - <br />"
-            Alarm = True
-        End If
+  ' ======================================================================
+  ' Variables
+  ' ======================================================================
+  Dim Body  As String = ""
+  Dim Alarm as Boolean = False
 
-        ' ======================================================================
-        ' Check the patio door
-        ' ======================================================================
-        If (hs.DeviceValue("68") <> 0) Then
-            Body = Body & " - Patio door is open - <br />"
-            Alarm = True
-        Else
-            Body = Body & "Patio door is closed<br />"
-        End If
+  ' ==========================================================================
+  ' Initial Checks
+  ' ==========================================================================
+  ' If Security Current Mode <> Disarmed
+  If ( hs.DeviceValue("28") <> 0 ) Then
 
-
-        ' ======================================================================
-        ' Armed Only Devices
-        ' ======================================================================
-        ' These devices will be included in the report, but will only trigger an
-        ' alarm if the system is fully armed.
-        ' ======================================================================
-        ' Family Room Motion Sensor
-        ' ======================================================================
-        If (hs.DeviceValue("112") <> 0) Then
-            Body = Body & "- Motion in Family Room -<br />"
-            If (hs.DeviceValue("28") = 100) Then
-                Alarm = True
-            End If
-        Else
-            Body = Body & "No motion in Family Room<br />"
-        End If
-
+    ' ======================================================================
+    ' Armed and Perimeter Devices
+    ' ======================================================================
+    ' These devices will always trigger an alarm if they are changed when
+    ' the system is not disarmed.
+    ' ======================================================================
+    ' Check the garage door
+    ' ======================================================================
+    If ( hs.DeviceValue("72") <> 0 ) Then
+      Body = Body & " - Garage door is open - <br />"
+      Alarm = True
     Else
-            hs.WriteLog("Security System", "Security system event triggered. System is disarmed. No alert sent.")
-	End If
-	
-	' ======================================================================
-	' Alert
-	' ======================================================================
-	If ( Alarm = True ) Then
-		' Set the alarm device to Intrusion
-		If ( hs.DeviceValue("92") <> 100 ) Then
-			hs.SetDeviceValueByRef("92",100,True)
-		End If
-	
-		' Send email
-		Body = "<br />" & Body & "<br />" & DateTime.Now.ToString("MMMM dd, HH:mm:ss")
-		hs.WriteLog("Security System", "Security system triggered an alert.")
-		
-		hs.SendEmail(hs.DeviceString(168), hs.DeviceString(174),"","", "Security System Alert", Body,"")
-		hs.SendEmail(hs.DeviceString(167), hs.DeviceString(174),"","", "Security System Alert", Body,"")
-	Else
-		hs.WriteLog("Security System", "Security system event triggered. All systems secured. No alert sent.")
-	End If
+      Body = Body & "Garage door is closed<br />"
+    End If
 
-end Sub
+    ' ======================================================================
+    ' Check the front door
+    ' ======================================================================
+    If (hs.DeviceValue("201") = 255) Then
+      Body = Body & "Front door is locked<br />"
+    Else
+      Body = Body & " - Front door is unlocked - <br />"
+      Alarm = True
+    End If
+
+    ' ======================================================================
+    ' Check the patio door
+    ' ======================================================================
+    If (hs.DeviceValue("68") <> 0) Then
+      Body = Body & " - Patio door is open - <br />"
+      Alarm = True
+    Else
+      Body = Body & "Patio door is closed<br />"
+    End If
+
+
+    ' ======================================================================
+    ' Armed Only Devices
+    ' ======================================================================
+    ' These devices will be included in the report, but will only trigger an
+    ' alarm if the system is fully armed.
+    ' ======================================================================
+    ' Family Room Motion Sensor
+    ' ======================================================================
+    If (hs.DeviceValue("112") <> 0) Then
+      Body = Body & "- Motion in Family Room -<br />"
+      If (hs.DeviceValue("28") = 100) Then
+        Alarm = True
+      End If
+    Else
+      Body = Body & "No motion in Family Room<br />"
+    End If
+
+  Else
+    hs.WriteLog("Security System", "Security system event triggered. System is disarmed. No alert sent.")
+  End If
+
+  ' ======================================================================
+  ' Alert
+  ' ======================================================================
+  If ( Alarm = True ) Then
+    ' Set the alarm device to Intrusion
+    If ( hs.DeviceValue("92") <> 100 ) Then
+      hs.SetDeviceValueByRef("92",100,True)
+    End If
+
+    ' Send email
+    Body = "<br />" & Body & "<br />" & DateTime.Now.ToString("MMMM dd, HH:mm:ss")
+    hs.WriteLog("Security System", "Security system triggered an alert.")
+
+    hs.SendEmail(hs.DeviceString(168), hs.DeviceString(174),"","", "Security System Alert", Body,"")
+    hs.SendEmail(hs.DeviceString(167), hs.DeviceString(174),"","", "Security System Alert", Body,"")
+  Else
+    hs.WriteLog("Security System", "Security system event triggered. All systems secured. No alert sent.")
+  End If
+
+End Sub
