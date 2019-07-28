11d8f710675e6dad650a1f565ed5d8cc9e023df1
Derek Winters <git@derekwinters.com>
2019-07-28 00:08:23 -0500

    Add new motion sensors

--- standalone-scripts/Security/TrackerManagment-Motion.vb
+++ standalone-scripts/Security/TrackerManagment-Motion.vb
@@ -3,10 +3,10 @@ Sub Main(Parms as Object)
 	' Variables
 	' ==========================================================================
 	' Monitors
-	Dim Sensors() As Integer = {112,215}
+	Dim Sensors() As Integer = {112,213,231,313,329,335,341}
 	
 	' Trackers
-	Dim Tracker        As String = "117"
+	Dim Tracker As String = "117"
 	
 	' ==========================================================================
 	' Check Sensors
@@ -16,13 +16,12 @@ Sub Main(Parms as Object)
 	
 	' Loop through sensors
 	For Each Sensor As Integer In Sensors
-		' If the sensor is On-Open-Motion
-		If ( hs.DeviceValue(Sensor) = 255 ) Then
+		' If the sensor is On-Open-Motion (255) or Motion Detected (8)
+		If ( hs.DeviceValue(Sensor) = 255 or hs.DeviceValue(Sensor) = 8 ) Then
 			Value = 100
 		End If
 	Next
 
 	' Set the Motion Tracker
 	hs.SetDeviceValueByRef(Tracker,Value,True)
-	
 End Sub
