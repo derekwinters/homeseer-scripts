fc27112a722da60673e5b1b8cf16742bcf68c72c
Derek Winters <git@derekwinters.com>
2019-10-02 15:13:51 -0500

    Daily water tracking

---
+++ standalone-scripts/RainMachine/DailyWaterTracking.vb
@@ -0,0 +1,15 @@
+' ==============================================================================
+' This runs every hour to track the amount of rain received. The weatherXML
+' device used as a reference tracks water in whole inches with an accuracy of
+' two decimal places.
+' ==============================================================================
+Sub Main (Param As Object)
+  Dim WaterLastHour As Double = hs.DeviceValue(381)
+  Dim Day0 As Integer = hs.DeviceValue(414)
+
+  ' Calculate new value
+  Day0 = Day0 + Math.Round((WaterLastHour*10),0,MidpointRounding.AwayFromZero)
+
+  ' Set the new value
+  hs.SetDeviceValueByRef((414),Day0,True)
+End Sub
