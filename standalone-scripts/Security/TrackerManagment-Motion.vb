929972410f646928a8ec02808234896c91d70a2b
Derek Winters <git@derekwinters.com>
2019-10-25 20:53:50 -0500

    fix tracker list

--- standalone-scripts/Security/TrackerManagment-Motion.vb
+++ standalone-scripts/Security/TrackerManagment-Motion.vb
@@ -1,27 +1,29 @@
-Sub Main(Parms as Object)
-	' ==========================================================================
-	' Variables
-	' ==========================================================================
-	' Monitors
-	Dim Sensors() As Integer = {112,213,231,313,329,335,341}
-	
-	' Trackers
-	Dim Tracker As String = "117"
-	
-	' ==========================================================================
-	' Check Sensors
-	' ==========================================================================
-	' Set the default to No Motion (0)
-	Dim Value As String = 0
-	
-	' Loop through sensors
-	For Each Sensor As Integer In Sensors
-		' If the sensor is On-Open-Motion (255) or Motion Detected (8)
-		If ( hs.DeviceValue(Sensor) = 255 or hs.DeviceValue(Sensor) = 8 ) Then
-			Value = 100
-		End If
-	Next
+Sub Main(Parms As Object)
+  ' ==========================================================================
+  ' Variables
+  ' ==========================================================================
+  ' Monitors
+  Dim Sensors() As Integer = {341,439,463,458,335,329,213}
 
-	' Set the Motion Tracker
-	hs.SetDeviceValueByRef(Tracker,Value,True)
+  ' Trackers
+  Dim Tracker As String = "117"
+
+  ' ==========================================================================
+  ' Check Sensors
+  ' ==========================================================================
+  ' Set the default to No Motion (0)
+  Dim Value As String = "0"
+
+  ' Loop through sensors
+  For Each Sensor As Integer In Sensors
+    ' Some motion sensors only set Home Security to Motion Detected (8), but
+    ' others set Binary Motion to (255) and Home Security to (8). Check if
+    ' each sensor is Motion Detected (8).
+    If hs.DeviceValue(Sensor) = 8 Then
+      Value = 100
+    End If
+  Next
+
+  ' Set the Motion Tracker
+  hs.SetDeviceValueByRef(Tracker,Value,True)
 End Sub
