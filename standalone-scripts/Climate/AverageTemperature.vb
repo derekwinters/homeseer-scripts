b133cf417adc1bf4597c84ae830c456260c28a8d
Derek Winters <git@derekwinters.com>
2019-10-28 22:25:37 -0500

    only track inside sensors

--- standalone-scripts/Climate/AverageTemperature.vb
+++ standalone-scripts/Climate/AverageTemperature.vb
@@ -1,81 +1,83 @@
 Sub Main(Parms As Object)
-    ' ==========================================================================
-    ' Variables
-    ' ==========================================================================
-    ' Find sensors that are "Z-Wave Temperature" and count the total
-    Dim Device As Scheduler.Classes.DeviceClass
-    Dim Enumerator As Scheduler.Classes.clsDeviceEnumeration
+  ' ==========================================================================
+  ' Variables
+  ' ==========================================================================
+  ' Find sensors that are "Z-Wave Temperature" and count the total
+  Dim Device As Scheduler.Classes.DeviceClass
+  Dim Enumerator As Scheduler.Classes.clsDeviceEnumeration
 
-    ' Math
-    Dim Sum As Double
-    Dim Total As Integer
-    Dim SumDownstairs As Double
-    Dim SumUpstairs As Double
-    Dim TotalDownstairs As Integer
-    Dim TotalUpstairs As Integer
-    Dim SumBedrooms As Double
-    Dim TotalBedrooms As Double
+  ' Math
+  Dim Sum As Double
+  Dim Total As Integer
+  Dim SumDownstairs As Double
+  Dim SumUpstairs As Double
+  Dim TotalDownstairs As Integer
+  Dim TotalUpstairs As Integer
+  Dim SumBedrooms As Double
+  Dim TotalBedrooms As Double
 
-    ' Trackers
-    Dim Tracker As String = "118"
-    Dim Upstairs As String = "324"
-    Dim Downstairs As String = "325"
-    Dim Bedrooms As String = "351"
+  ' Trackers
+  Dim Tracker As String = "118"
+  Dim Upstairs As String = "324"
+  Dim Downstairs As String = "325"
+  Dim Bedrooms As String = "351"
 
-    Enumerator = hs.GetDeviceEnumerator
+  Enumerator = hs.GetDeviceEnumerator
 
-    If (Enumerator Is Nothing) Then
-        hs.WriteLog("HVAC Automation", "Error getting enumerator")
-        Exit Sub
+  If (Enumerator Is Nothing) Then
+    hs.WriteLog("HVAC Automation", "Error getting enumerator")
+    Exit Sub
+  End If
+
+  Do While Not Enumerator.Finished
+    Device = Enumerator.GetNext
+
+    If (Device Is Nothing) Then
+      Continue Do
     End If
 
-    Do While Not Enumerator.Finished
-        Device = Enumerator.GetNext
+    If (Device.Device_Type_String(hs) = "Z-Wave Temperature") Then
+      If ( hs.DeviceName(Device.ref(hs)).StartsWith("Upstairs") ) Then
+        hs.WriteLog("HVAC Automation", "Found a temperature device (Name: " & hs.DeviceName(Device.ref(hs)) & ", Value: " & hs.DeviceValueEx(Device.ref(hs)) & ")")
+        SumUpstairs = SumUpstairs + hs.DeviceValueEx(Device.ref(hs))
+        TotalUpstairs = TotalUpstairs + 1
+        Sum = Sum + hs.DeviceValueEx(Device.ref(hs))
+        Total = Total + 1
+      ElseIf ( hs.DeviceName(Device.ref(hs)).StartsWith("Downstairs") ) Then
+        hs.WriteLog("HVAC Automation", "Found a temperature device (Name: " & hs.DeviceName(Device.ref(hs)) & ", Value: " & hs.DeviceValueEx(Device.ref(hs)) & ")")
+        SumDownstairs = SumDownstairs + hs.DeviceValueEx(Device.ref(hs))
+        TotalDownstairs = TotalDownstairs + 1
+        Sum = Sum + hs.DeviceValueEx(Device.ref(hs))
+        Total = Total + 1
+      End If
 
-        If (Device Is Nothing) Then
-            Continue Do
-        End If
+      If (hs.DeviceName(Device.ref(hs)).Contains("Bedroom")) Then
+        SumBedrooms = SumBedrooms + hs.DeviceValueEx(Device.ref(hs))
+        TotalBedrooms = TotalBedrooms + 1
+      End If
+    End If
 
-        If (Device.Device_Type_String(hs) = "Z-Wave Temperature") Then
-            hs.WriteLog("HVAC Automation", "Found a temperature device (Name: " & hs.DeviceName(Device.ref(hs)) & ", Value: " & hs.DeviceValueEx(Device.ref(hs)) & ")")
-            Sum = Sum + hs.DeviceValueEx(Device.ref(hs))
-            Total = Total + 1
+  Loop
 
-            If ( hs.DeviceName(Device.ref(hs)).StartsWith("Upstairs") ) Then
-              SumUpstairs = SumUpstairs + hs.DeviceValueEx(Device.ref(hs))
-              TotalUpstairs = TotalUpstairs + 1
-            ElseIf ( hs.DeviceName(Device.ref(hs)).StartsWith("Downstairs") ) Then
-              SumDownstairs = SumDownstairs + hs.DeviceValueEx(Device.ref(hs))
-              TotalDownstairs = TotalDownstairs + 1
-            End If
+  ' ==========================================================================
+  ' Math
+  ' ==========================================================================
+  Dim Average As Double = Sum / Total
+  Dim AverageDownstairs As Double = SumDownstairs / TotalDownstairs
+  Dim AverageUpstairs As Double = SumUpstairs / TotalUpstairs
+  Dim AverageBedrooms As Double = SumBedrooms / TotalBedrooms
 
-            If (hs.DeviceName(Device.ref(hs)).Contains("Bedroom")) Then
-              SumBedrooms = SumBedrooms + hs.DeviceValueEx(Device.ref(hs))
-              TotalBedrooms = TotalBedrooms + 1
-            End If
-        End If
+  'Log the calculated average before rounding and setting the value
+  hs.WriteLog("HVAC Automation", "Average home temperature is " & Average & " F, Downstairs " & AverageDownstairs & " F, Upstairs " & AverageUpstairs & " F, Bedrooms " & AverageBedrooms & " F" )
 
-    Loop
+  Average = Math.Round(Average,1,MidpointRounding.AwayFromZero)
+  AverageDownstairs = Math.Round(AverageDownstairs,1,MidpointRounding.AwayFromZero)
+  AverageUpstairs = Math.Round(AverageUpstairs,1,MidpointRounding.AwayFromZero)
+  AverageBedrooms = Math.Round(AverageBedrooms,1,MidpointRounding.AwayFromZero)
 
-    ' ==========================================================================
-    ' Math
-    ' ==========================================================================
-    Dim Average As Double = Sum / Total
-    Dim AverageDownstairs As Double = SumDownstairs / TotalDownstairs
-    Dim AverageUpstairs As Double = SumUpstairs / TotalUpstairs
-    Dim AverageBedrooms As Double = SumBedrooms / TotalBedrooms
-	
-    'Log the calculated average before rounding and setting the value
-    hs.WriteLog("HVAC Automation", "Average home temperature is " & Average & " F, Downstairs " & AverageDownstairs & " F, Upstairs " & AverageUpstairs & " F, Bedrooms " & AverageBedrooms & " F" )
-	
-    Average = Math.Round(Average,1,MidpointRounding.AwayFromZero)
-    AverageDownstairs = Math.Round(AverageDownstairs,1,MidpointRounding.AwayFromZero)
-    AverageUpstairs = Math.Round(AverageUpstairs,1,MidpointRounding.AwayFromZero)
-    AverageBedrooms = Math.Round(AverageBedrooms,1,MidpointRounding.AwayFromZero)
-	
-    hs.SetDeviceValueByRef(Tracker, Average, True)
-    hs.SetDeviceValueByRef(Upstairs, AverageUpstairs, True)
-    hs.SetDeviceValueByRef(Downstairs, AverageDownstairs, True)
-    hs.SetDeviceValueByRef(Bedrooms, AverageBedrooms, True)
+  hs.SetDeviceValueByRef(Tracker, Average, True)
+  hs.SetDeviceValueByRef(Upstairs, AverageUpstairs, True)
+  hs.SetDeviceValueByRef(Downstairs, AverageDownstairs, True)
+  hs.SetDeviceValueByRef(Bedrooms, AverageBedrooms, True)
 
 End Sub
